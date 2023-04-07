42015203ea6d99170606bb978b1e86a482356a57
wendellopes <wendelopes@msn.com>
2023-04-07 17:36:52 -0300

    created balance component

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Home from './src/pages/Home';
 
 export default function App() {
   return (
-      <View className="flex-1 items-start justify-start">
+      <View className="flex-1 items-start justify-start bg-gray-50">
         <Home />
       </View>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Header from '../components/Header';
+import Balence from '../components/Balence';
 
 export default () => {
   return (
     <View className="w-full">
       <Header name="Wendel Lopes" />
-      <Text className=""></Text>
+      <Balence balence={9756.82} expenses={-765.32} />
     </View>
   )
 };
