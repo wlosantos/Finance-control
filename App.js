87200b0db6ebe844f5e7dac082a3b7e8a6cd385b
wendellopes <wendelopes@msn.com>
2023-04-07 15:40:49 -0300

    setting initial project

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black p-4">
-      <Text className="text-2xl font-bold text-cyan-500">nativeWind!</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center bg-black p-4">
+        <Text className="text-4xl font-bold text-cyan-500">W3n Mobile</Text>
+      </View>
+    </SafeAreaView>
   );
 }
